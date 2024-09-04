80a875643eec19b3a4eef9acb27f0d90d13be7ea
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-04 08:51:09 +0700

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+      register: true,
+      scope: "/app",
+      sw: "service-worker.js",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
