5b0c025eff1674d2bd7b943c04c47f3fa2e052a1
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-05 14:20:15 +0700

    add scss

--- components/Test.tsx
+++ components/Test.tsx
@@ -3,6 +3,7 @@ import { decrement, increment } from "@/store/counter/counterSlice";
 import { RootState } from "@/store/store";
 import { Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
+import style from "@/styles/test.module.scss"
 
 function Test() {
   const count = useSelector((state: RootState) => state.counter.value)
@@ -12,7 +13,7 @@ function Test() {
       <Button danger onClick={() => dispatch(decrement())}>
         minus
       </Button>
-      <span className="flex justify-center">{count}</span>
+      <span className={style.name}>{count}</span>
       <Button onClick={() => dispatch(increment())}>
         plus
       </Button>
