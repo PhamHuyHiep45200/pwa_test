8b39a7c1d4775d20a29501adc664476d74cb24a3
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-06 10:12:16 +0700

    fix api

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 /* eslint-disable no-console */
 import Test from "@/components/Test";
-import { getData } from "@/requests/test";
 
 export default async function Home() {
-  try {
-    const res = await getData()
-    console.log("res", res);
-
-  } catch (error) {
-    console.log("error", error);
-  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Test />
