4c4ee9f52be0eafbc1256fe0fd58908bd8915ecc
Huy Hiep <hiepph3@rikkeisoft.com>
2024-09-09 14:09:38 +0700

    add hook

---
+++ hooks/useWindowSize.ts
@@ -0,0 +1,21 @@
+import { SizeWindowType } from "@/models/hooks";
+import { useEffect, useState } from "react";
+
+// when resize screen return width height screen device 
+const useWindowSize = () => {
+  const [size, setSize] = useState<SizeWindowType>({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    function updateSize() {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+};
+
+export default useWindowSize
